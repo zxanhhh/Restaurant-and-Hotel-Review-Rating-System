bbd7efcb70bc066d8bcc40cbc08c347c9f9d77a0
Nguyễn Xuân Anh <23000091@hus.edu.vn>
2026-06-06 12:31:34 +0700

    Create  deps.py

---
+++ api/deps.py
@@ -0,0 +1,15 @@
+from typing import Generator
+from sqlalchemy.orm import Session
+from db.database import SessionLocal
+
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+        db.commit()
+    except Exception:
+        db.rollback()
+        raise
+    finally:
+        db.close()
